2d6995aac0fb9d6f43fc25fec83596db62e448cb
Yukari Sakurai <neko3genki@gmail.com>
2017-03-07 20:55:32 +0900

    Add Privacy Policy

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get 'static_pages/home'
+  get 'static_pages/privacy_policy'
 
   resources :foods
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,4 +6,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get privacy_policy" do
+    get static_pages_privacy_policy_url
+    assert_response :success
+  end
+
 end
