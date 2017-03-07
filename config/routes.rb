8bbf8fa3d78df77617ff1f91e61be8ff18f6be3f
Yukari Sakurai <neko3genki@gmail.com>
2017-03-07 13:07:20 +0900

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   resources :foods
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'foods#index'
