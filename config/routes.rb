547a306c068bbc7adc713b52faefce5c28cecb7f
Yukari Sakurai <neko3genki@gmail.com>
2017-03-07 20:14:45 +0900

    Improve some UI parts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'static_pages/home'
 
   resources :foods
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'foods#index'
 end
