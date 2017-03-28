ffb7179a2752b338672bdc09537ae438144d3bf0
Yukari Sakurai <neko3genki@gmail.com>
2017-03-28 23:20:08 +0900

    Change text color by carbohydrate rate

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -1,2 +1,18 @@
 module FoodsHelper
+
+  # 糖質の量によって文字色を変更する
+  def carbo_level_color_css(food)
+
+    case food.carbohydrate_per_100g
+      when 0 .. 5 then
+        # 糖質量が少ない
+        return "col-md-3 food_grid_left_safe"
+      when 5 .. 15 then
+        # 糖質量がやや多い
+        return "col-md-3 food_grid_left_warning"
+      else
+        # 糖質量が多い
+        return "col-md-3 food_grid_left_danger"
+    end
+  end
 end
