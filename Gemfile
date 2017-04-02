f51cc7428a81547ec1565d243487c146e515ddd3
Yukari Sakurai <neko3genki@gmail.com>
2017-04-02 17:31:52 +0900

    Improve around meta tags using gem meta-tags

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.4.1'
 gem 'pg'
 gem 'google-analytics-rails', '1.1.0'
+gem 'meta-tags'
 gem 'config'
 
 group :development, :test do
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def default_meta_tags
+    {
+        reverse: true,
+        description: Settings.site.description,
+        canonical: Settings.site.url,
+        og: {
+            title: :title,
+            type: Settings.site.og.type,
+            url: Settings.site.url,
+            image: default_image_url,
+            site_name: Settings.site.name,
+            description: :description,
+            locale: 'ja_JP'
+        }
+    }
+  end
+
+  def default_image_url
+    image_url('kind1.jpg')
+  end
 end
