4dc04167950215db1542a2ccc4c18226a786d52a
Yukari Sakurai <neko3genki@gmail.com>
2017-04-04 01:53:23 +0900

    Move Google adsense code to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,33 @@ module ApplicationHelper
   def default_image_url
     image_url('kind1.jpg')
   end
+
+  def google_adsense
+    html = <<-EOF
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-6381489451756721",
+        enable_page_level_ads: true
+      });
+    </script>
+    EOF
+    html.html_safe
+  end
+
+  # レスポンシブ広告枠
+  def google_adsense_unit1
+    html = <<-EOF
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-6381489451756721"
+         data-ad-slot="6685554091"
+         data-ad-format="auto"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    EOF
+    html.html_safe
+  end
 end
