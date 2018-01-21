c9d3aa75222dae02b3f7eae8154478128bc4e4dc
Yukari Sakurai <neko3genki@gmail.com>
2018-01-21 14:04:16 +0900

    Update Type / Kind / Food data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,24 @@ Food.delete_all
 Kind.delete_all
 Type.delete_all
 
-csv_data = CSV.read('db/type.csv', headers: true)
+csv_data = CSV.read('db/type.csv', headers: true,
+  external_encoding: "Shift_JIS",
+  internal_encoding: "utf-8")
+
 csv_data.each do |data|
   Type.create!(data.to_hash)
 end
 
-csv_data = CSV.read('db/kind.csv', headers: true)
+csv_data = CSV.read('db/kind.csv', headers: true,
+    external_encoding: "Shift_JIS",
+    internal_encoding: "utf-8")
 csv_data.each do |data|
   Kind.create!(data.to_hash)
 end
 
 csv_data = CSV.read('db/food.csv', headers: true,
     external_encoding: "Shift_JIS",
-    internal_encoding: "utf-8"
-  )
+    internal_encoding: "utf-8")
 csv_data.each do |data|
   Food.create!(data.to_hash)
 end
