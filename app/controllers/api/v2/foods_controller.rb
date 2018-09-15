3d6454c2669bce1cac2af7ba8bcde57936a272ed
Yukari Sakurai <neko3genki@gmail.com>
2018-09-16 02:22:07 +0900

    Update api/v2/foods (Add 'notes' to the result data)

--- app/controllers/api/v2/foods_controller.rb
+++ app/controllers/api/v2/foods_controller.rb
@@ -9,7 +9,7 @@ module Api
         data_version = Settings.foods.data_version
         kinds = Kind.select('id, name, search_word, type_id')
         foods = Food.select('id, name, carbohydrate_per_100g, carbohydrate_per_weight, weight,
-                           weight_hint, calory, protein, fat, sodium, search_word,
+                           weight_hint, calory, protein, fat, sodium, search_word, notes,
                             type_id, kind_id')
                     .where(deleted_flg: false)
         hash = { :data_version => data_version, :kinds => kinds, :foods => foods }
