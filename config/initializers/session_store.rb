672db20bd3d7091f8b9639bdddb03c28b932b3b4
Yukari Sakurai <neko3genki@gmail.com>
2018-11-28 12:00:04 +0900

    Force all access to the app over SSL

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_pocket_carbo_session'
+Rails.application.config.session_store :cookie_store,
+  key: '_pocket_carbo_session',
+  secure: Rails.env.production?
