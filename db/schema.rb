c452e62f80dcd0cfeb2f36938ae586785c3e9894
Yukari Sakurai <neko3genki@gmail.com>
2018-12-21 14:24:56 +0900

    Update Types column to NOT NULL

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_15_154851) do
+ActiveRecord::Schema.define(version: 2018_12_21_042451) do
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2018_09_15_154851) do
   end
 
   create_table "types", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
